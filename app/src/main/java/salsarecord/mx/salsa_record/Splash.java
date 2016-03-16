cf12d9024605e08a6d847c9a232906bb7b5369ee
luisiy0 <luishandball2@hotmail.com>
2016-03-16 10:04:38 -0600

    splash done

---
+++ app/src/main/java/salsarecord/mx/salsa_record/Splash.java
@@ -0,0 +1,55 @@
+package salsarecord.mx.salsa_record;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
+import android.preference.PreferenceManager;
+import android.view.Window;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        // Hide title bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        super.onCreate(savedInstanceState);
+        // Set portrait orientation
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        setContentView(R.layout.activity_splash);
+
+        getSupportActionBar().hide();
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                // Start the next activity
+                Intent mainIntent = new Intent().setClass(
+                        Splash.this, MainActivity.class);
+                startActivity(mainIntent);
+                // Close the activity so the user won't able to go back this
+                // activity pressing Back button
+                finish();
+            }
+        };
+
+        // Simulate a long loading process on application startup.
+        Timer timer = new Timer();
+        timer.schedule(task, 2000); // 1 segundo
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+}
